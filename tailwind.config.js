6fbaac96ed7e27e2f698fbeffa487806362a6536
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-06-04 17:49:06 +0200

    feat - add packages + tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6257",
+        secondary: "#242742",
+        accent: "#36384E",
+        grey: "#9294A0",
+        white: "#FFFFFF",
+      },
+      fontFamily: {
+        Roboto: ["Roboto, sans-serif"],
+      },
+      fontSize: {
+        'p': '16px',
+      },
+      screens: {
+        xs: "300px",
+        sm: "600px",
+        md: "900px",
+        lg: "1200px",
+        xl: "1536px",
+      }
+    },
+  },
+  plugins: [],
+}
+
