1395627c45729445c70879a5fbca29db81f5aba6
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-06-04 17:50:21 +0200

    feat - add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,38 @@
+<template>
+    <button :class="`text-white ${backgroundColor} ${width} rounded-md p-3`" @click="onClick">
+        {{ title }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            default: 'button'
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        onClick: {
+            type: Function,
+            required: true,
+            default: () => { }
+        },
+        backgroundColor: {
+            type: String,
+            default: 'bg-secondary'
+        },
+        width: {
+            type: String,
+            default: 'w-full'
+        },
+    }
+}
+</script>
