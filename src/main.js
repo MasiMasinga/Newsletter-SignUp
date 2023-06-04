0277568533b3e6f048b2015fc4b5cda6a95ad309
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-06-04 17:51:25 +0200

    feat - initialize reusable components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Styles
+import './styles/tailwind.css';
+
+// Components
+import Button from './components/Button.vue';
+import InputField from './components/InputField.vue';
+import InputFieldLabel from './components/InputFieldLabel.vue';
+import Typography from './components/Typography.vue';
+
+const app = createApp(App)
+
+app.component('Button', Button);
+app.component('InputField', InputField);
+app.component('InputFieldLabel', InputFieldLabel);
+app.component('Typography', Typography);
+
+app.mount('#app')
