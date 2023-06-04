437a9bd5897137131d2f14c73259ffd786a83613
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-06-04 17:50:45 +0200

    feat - add input field component

---
+++ src/components/InputFieldLabel.vue
@@ -0,0 +1,41 @@
+<template>
+    <label :class="`${textColor} ${fontSize} ${fontWeight} ${textAlign} ${marginTop} ${marginBottom}`">
+        {{ label }}
+    </label>
+</template>
+
+<script>
+export default {
+    name: 'InputFieldLabel',
+    props: {
+        label: {
+            type: String,
+            required: true
+        },
+        fontSize: {
+            type: String,
+            default: 'text-base'
+        },
+        fontWeight: {
+            type: String,
+            default: 'font-normal'
+        },
+        textColor: {
+            type: String,
+            default: 'text-black'
+        },
+        textAlign: {
+            type: String,
+            default: 'text-left'
+        },
+        marginTop: {
+            type: String,
+            default: 'mt-0'
+        },
+        marginBottom: {
+            type: String,
+            default: 'mb-0'
+        },
+    }
+}
+</script>
